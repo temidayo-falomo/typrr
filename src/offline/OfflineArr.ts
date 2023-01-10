b2242e2c4f45c313c90dfb4495271b6c6d7007e3
temidayo <temmideee@gmail.com>
2023-01-10 23:41:59 +0100

    updated offline text

--- src/offline/OfflineArr.ts
+++ src/offline/OfflineArr.ts
@@ -1,18 +1,65 @@
 export const offlineArr = [
-  `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur ad
-    distinctio dolores modi vel, officiis impedit! Maiores adipisci corporis
-    magnam cumque neque No matter how beautiful the sunset, it saddened her.`,
+  `Vel pretium lectus quam id leo. Nunc mattis enim ut tellus elementum. Dui id ornare arcu odio. 
+  Mauris a diam maecenas sed enim ut sem viverra. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. 
+  Porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. 
+  Laoreet non curabitur gravida arcu ac tortor dignissim convallis. A scelerisque purus semper eget duis. Ante in nibh mauris cursus.
+   Sollicitudin. Turpis egestas maecenas pharetra convallis posuere morbi. 
+   Integer eget aliquet nibh praesent tristique magna.
+    Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula. Lacus sed turpis tincidunt id aliquet risus feugiat.`,
 
-  `You can't change the past, but you can ruin 
-    the present by worrying about the future. 
-    So just live in the moment and stop worrying about the future.`,
+  `And I love it when you watch me sleep
+  You spin me 'round so I can breathe
+  It's only safe for you and me
+  I know you won't let me OD
+  And if I finally die in peace
+  Just wrap my body in these sheets
+  And pour out the gasoline
+  It don't mean much to me
+  It's five AM, I'm nihilist
+I know there's nothing after this (after this)
+Obsessing over aftermaths
+Apocalypse and hopelessness (hopelessness)
+The only thing I understand
+Is zero sum of tenderness (tenderness)
+Oh, baby, please just hold me close
+Make me believe there's more to live
+  `,
 
   `The best way to predict the future is to create it.
   No matter how beautiful the sunset, it saddened her knowing she was one day older.
   After exploring the abandoned building, he started to believe in ghosts.
-  The door swung open to reveal pink giraffes and red elephants..`,
+  The door swung open to reveal pink giraffes and red elephants..
+  Malesuada fames ac turpis egestas sed. Aliquam eleifend mi in nulla posuere
+  Apocalypse and hopelessness
+The only thing I understand
+Is zero sum of tenderness
+Oh, baby, please just hold me close
+Make me believe there's more to live
+  `,
 
-  `The future belongs to those who believe in the beauty of their dreams.`,
+  `My darkest hours
+  Girl, I felt so alone inside of this crowded room
+  Different girls on the floor, distracting my thoughts of you
+  I turned into the man I used to be, to be
+  Put myself to sleep
+  Just so I can get closer to you inside my dreams
+  Didn't wanna wake up 'less you were beside me
+  I just wanted to call you and say, and say
+  Oh, baby, where are you now when I need you most?
+  I'd give it all just to hold you close
+  Sorry that I broke your heart, your heart
+  Never comin' down, uh
+  I was runnin' away from facin' reality, uh
+  Wastin' all of my time on livin' my fantasies`,
 
-  `The future is something which everyone reaches at the rate of sixty minutes`,
+  `So I love when you call unexpected
+  'Cause I hate when the moment's expected
+  So I'ma care for you, you, you
+  I'ma care for you, you, you, you, yeah
+  'Cause girl you're perfect (Girl, you're perfect)
+  You're always worth it (You're always worth it)
+  And you deserve it (And you deserve it)
+  The way you work it (The way you work it)
+  'Cause girl you earned it
+  Girl you earned it, yeah`,
 ];
